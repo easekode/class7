cf5e93124c39854404ef3a5f9ef3843f334bd946
thabungm <thabungm@gmail.com>
2023-10-20 17:32:59 +0530

    adding changes

---
+++ javascript/1.dataTypes/0.dataTypes.js
@@ -0,0 +1,39 @@
+/*
+steps in preparing tea
+inputs: water, tea-powder, sugar(taste, color, quantity, form)
+output: tea
+
+    1. boil water
+    2. put tea in cup
+    3. pour water in cup
+    4. add sugar
+    5. stir
+    6. drink
+
+*/
+/*
+pan contains water
+pan contains milk
+tea-bag contains tea
+*/
+// string
+var message = 'hello';
+var name = 'John';
+var courseName = 'MERN stack';
+// console.log(message, name, courseName);
+
+// number
+var age = 10; // integer
+var amount = 10.5; // float
+
+// console.log(age);
+// console.log(amount);
+// boolean
+var isCompleted = true;
+var isGraduated = false;
+// console.log(isCompleted);
+var earthIn2025;
+// undefined:it is declared without value
+// console.log(earthIn2025);
+var emptyBox = null;
+console.log(emptyBox);
