c4dadc03a32e9a43adb69af731d7c78d92c0858a
thabungm <thabungm@gmail.com>
2023-10-22 17:44:16 +0530

    adding changes

---
+++ javascript/1.dataTypes/exercises/findVowels.js
@@ -0,0 +1,10 @@
+function findVowels() {
+  var arr = ['a', 'e', 'x', 'o'];
+  //0,3, 4
+  for (i = 0; i <= arr.length - 1; i++) {
+    // check if arr[i] is one of <- a, e, i, o, u
+    // print that arr[i] is vowel
+  }
+}
+
+findVowels();
---
+++ javascript/2.loops/3.conditions.js
@@ -0,0 +1,45 @@
+// conditional operators
+/*
+> , greater than
+< , less than
+== , equals to 
+=== , equals to with type checking
+>= , greater than or equal to
+<= , less than or equal to
+!= , not equal to
+!== , not equal to with type checking
+*/
+/* 
+if(condition is true) {
+
+} 
+*/
+/* var age = 17;
+if (age >= 18) {
+  console.log('You are eligible to vote');
+} else if (age >= 16) {
+  console.log('Come back next year');
+} else {
+  console.log('You are not eligible to vote');
+} */
+
+// ternary operator
+/* var age = 17;
+var result = age > 18 ? 'eligible to vote' : 'not eligible to vote';
+console.log(result); */
+
+// switch case
+var day = 'Tuesday';
+switch (day) {
+  case 'Monday':
+    console.log('Today is Monday');
+    break;
+  case 'Tuesday':
+    console.log('Today is Tuesday');
+    break;
+  default:
+    console.log('invalid day');
+}
+
+//vowels = ['a', 'e', 'i', 'o', 'u'];
+// letters = ['a','b','c','d']
